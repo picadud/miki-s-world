47d4542b9d2cf8e02a8a91cd31597e55a035d982
tianxiefuzz <wingyy130@gmail.com>
2024-06-20 15:33:35 +0800

    Add a keybind function. Players can now remap keys from the keybind menu, and the keybind settings will be saved in the config file.

--- gui/ConfigFileHandler.gd
+++ gui/ConfigFileHandler.gd
@@ -8,20 +8,29 @@ const CONFIG_FILE_PATH = "user://config.cfg"
 
 func _ready():
 	config.load(CONFIG_FILE_PATH)
-	if !config.has_section("audio"):
+	if !config.has_section("audio") or !config.has_section("keybinds"):
 		config_init()
-		print("初始化了声音设置")
+		print("init settings")
 		
 		
 func config_init():
-	var dict = {
+	var dict_volume = {
 		"master_volume":0.8,
 		"bgm_volume":0.8,
 		"sfx_volume":0.8
 	}
 	
-	for key in dict.keys():
-		config.set_value("audio", key, dict[key])
+	var dict_keybinds = {
+		"jump":4194320,
+		"move_left":4194319,
+		"move_right":4194321
+	}
+	
+	for key in dict_volume.keys():
+		config.set_value("audio", key, dict_volume[key])
+	
+	for key in dict_keybinds.keys():
+		config.set_value("keybinds", key, dict_keybinds[key])
 		
 	config.save(CONFIG_FILE_PATH)
 
@@ -36,3 +45,13 @@ func load_audio_settings():
 	for key in config.get_section_keys("audio"):
 		audio_settings[key] = config.get_value("audio", key)
 	return audio_settings
+
+func save_keybinds_setting(key:String, value):
+	config.set_value("keybinds", key, value)
+	config.save(CONFIG_FILE_PATH)
+	
+func load_keybinds_settings():
+	var keybinds_settings = {}
+	for key in config.get_section_keys("keybinds"):
+		keybinds_settings[key] = config.get_value("keybinds", key)
+	return keybinds_settings
